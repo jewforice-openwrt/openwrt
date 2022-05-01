e10636681e6f5991218fe94fa453f7314a70e31b
jewforice-openwrt <78063842+jewforice-openwrt@users.noreply.github.com>
2022-05-01 17:46:08 +0800

    Update diy-part2.sh
    解决containerd hash不一致的问题（尝试

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/PKG_HASH.*/PKG_HASH:=skip/' feeds/packages/utils/containerd/Makefile
+
+
 
 
